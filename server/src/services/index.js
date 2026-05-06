3df959e768547472d7ae754651715d4258c02d70
Devisingh Rajput <devisinghrajput.cg@gmail.com>
2026-05-06 22:57:47 +0530

    feat: export all services via central index

---
+++ server/src/services/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  authService: require('./auth.service'),
+  areaService: require('./area.service'),
+  scoreService: require('./score.service'),
+  reviewService: require('./review.service'),
+  externalApiService: require('./externalApi.service'),
+  amenityService: require('./amenity.service'),
+};
